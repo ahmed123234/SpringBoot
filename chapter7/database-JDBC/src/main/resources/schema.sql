9b9423a744f6eb2447ad9c437cf00c6da05c42be
Ahmad Ghannam <75845807+ahmed123234@users.noreply.github.com>
2022-08-08 10:54:34 +0200

    Update schema.sql

--- chapter7/database-JDBC/src/main/resources/schema.sql
+++ chapter7/database-JDBC/src/main/resources/schema.sql
@@ -1,9 +1,12 @@
-drop database if exists springbootdb;
+use test_schema;
 
-create database springbootdb;
-use springbootdb;
-drop table if exists users;
-create table users(
-                      id int UNSIGNED primary key not null auto_increment,
-                      name varchar(100),
-                      email varchar(100));
+create table user(
+username varchar(50) primary key,
+email varchar(50) not null,
+password varchar(10)not null,
+creation_date datetime
+);
+
+insert into user values("ahmad", "ahmad@gm,ail.com", "1234", now());
+insert into user values("ali", "ali@gm,ail.com", "12345", now());
+insert into user values("ayham", "ayham@gm,ail.com", "123456", now());
